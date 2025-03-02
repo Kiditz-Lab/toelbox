034a7603864564b8f4b7574796fef97716122078
rifkyaditya <kiditzbastara@gmail.com>
2025-03-02 22:40:08 +0700

    fix logo

--- frontend/src/layouts/dashboard/logo/LogoDark.vue
+++ frontend/src/layouts/dashboard/logo/LogoDark.vue
@@ -13,9 +13,6 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
-const primary = ref('rgb(var(--v-theme-primary))');
-const darkprimary = ref('rgb(var(--v-theme-darkprimary))');
-import logo from '@/assets/images/logo-text.svg';
+
 </script>
