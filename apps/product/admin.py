195639a8e354c36b666a22e2b013c344923b168f
rifkyaditya <kiditzbastara@gmail.com>
2025-03-07 01:09:24 +0700

    add admin

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name',)
+    ordering = ('-created_at',)
